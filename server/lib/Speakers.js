e382143e0cc34f473412e017599bed1c1cc0f48a
Shiyas <shiyas.cholamukhath@tabcorp.com.au>
2021-08-22 23:16:03 +1000

    handling async

--- server/lib/Speakers.js
+++ server/lib/Speakers.js
@@ -11,93 +11,93 @@ class SpeakersService {
   async getNames() {
     try {
       const data = await this.getData();
-      return data.map((speaker) => ({
-        name: speaker.name,
-        shortname: speaker.shortname,
-      }));
     } catch (error) {
       console.log(error);
     }
+    return data.map((speaker) => ({
+      name: speaker.name,
+      shortname: speaker.shortname,
+    }));
   }
 
   async getListShort() {
     try {
       const data = await this.getData();
-      return data.map((speaker) => ({
-        name: speaker.name,
-        shortname: speaker.shortname,
-        title: speaker.title,
-      }));
     } catch (error) {
       console.log(error);
     }
+    return data.map((speaker) => ({
+      name: speaker.name,
+      shortname: speaker.shortname,
+      title: speaker.title,
+    }));
   }
 
   async getList() {
     try {
       const data = await this.getData();
-      return data.map((speaker) => ({
-        name: speaker.name,
-        shortname: speaker.shortname,
-        title: speaker.title,
-        summary: speaker.summary,
-      }));
     } catch (error) {
       console.log(error);
     }
+    return data.map((speaker) => ({
+      name: speaker.name,
+      shortname: speaker.shortname,
+      title: speaker.title,
+      summary: speaker.summary,
+    }));
   }
 
   async getAllArtwork() {
     try {
       const data = await this.getData();
-      const artwork = data.reduce((acc, elm) => {
-        if (elm.artwork) {
-          // eslint-disable-next-line no-param-reassign
-          acc = [...acc, ...elm.artwork];
-        }
-        return acc;
-      }, []);
-      return artwork;
     } catch (error) {
       console.log(error);
     }
+    const artwork = data.reduce((acc, elm) => {
+      if (elm.artwork) {
+        // eslint-disable-next-line no-param-reassign
+        acc = [...acc, ...elm.artwork];
+      }
+      return acc;
+    }, []);
+    return artwork;
   }
 
   async getSpeaker(shortname) {
     try {
       const data = await this.getData();
-      const speaker = data.find((current) => current.shortname === shortname);
-      if (!speaker) return null;
-      return {
-        title: speaker.title,
-        name: speaker.name,
-        shortname: speaker.shortname,
-        description: speaker.description,
-      };
     } catch (error) {
       console.log(error);
     }
+    const speaker = data.find((current) => current.shortname === shortname);
+    if (!speaker) return null;
+    return {
+      title: speaker.title,
+      name: speaker.name,
+      shortname: speaker.shortname,
+      description: speaker.description,
+    };
   }
 
   async getArtworkForSpeaker(shortname) {
     try {
       const data = await this.getData();
-      const speaker = data.find((current) => current.shortname === shortname);
-      if (!speaker || !speaker.artwork) return null;
-      return speaker.artwork;
     } catch (error) {
       console.log(error);
     }
+    const speaker = data.find((current) => current.shortname === shortname);
+    if (!speaker || !speaker.artwork) return null;
+    return speaker.artwork;
   }
 
   async getData() {
     try {
       const data = await readFile(this.datafile, "utf8");
-      if (!data) return [];
-      return JSON.parse(data).speakers;
     } catch (error) {
       console.log(error);
     }
+    if (!data) return [];
+    return JSON.parse(data).speakers;
   }
 }
 
